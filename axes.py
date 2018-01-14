1e3f42b46c6c02be99514ddf157d2df490737484
cclauss <cclauss@bluewin.ch>
2018-01-14 09:58:34 +0100

    from PIL import ImageFont

--- axes.py
+++ axes.py
@@ -3,6 +3,7 @@ Methods for drawing primitive constructs like axes, grids, arrows, etc.
 '''
 
 import numpy as np
+from PIL import ImageFont
 
 '''
 Draws four axes in 4d space. If the fourth row and fourth column of the rotation matrix, r are identity-like (0,0,0,1), you will not see the fourth axis.
