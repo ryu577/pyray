3b05fb7420e9af2033ae0a8b2665bfb282b36e15
byorxyz <ohmyheroine@hotmail.com>
2018-01-29 12:10:24 -0800

    Added bouncy sphere.

--- examples.py
+++ examples.py
@@ -55,6 +55,22 @@ def draw_wavy_sphere_wrapper(save_dir, number_of_circles):
         im.save(file_name)
 
 
+def draw_wavy_sphere_accleration_wrapper(save_dir, number_of_circles):
+    for i in np.arange(60):
+        wavy_index = i % number_of_circles
+        # 2.52 is an angle of Z axis (Why do I choose 2.52? For an aesthetic reason:))
+        r = rotation(3, 2.50 + np.pi*np.sin(i/10.0) * np.random.uniform(0.8,1) / 30.0)
+        # Create the canvas size of (500, 500)
+        im = Image.new("RGB", (500, 500), (1, 1, 1))
+        draw = ImageDraw.Draw(im, 'RGBA')
+        # Sphere's center is np.array([0,0,0])
+        # The vector that passes through the center is np.array([0,0,1])
+        # Radius is oscillating randomly
+        draw_sphere2(draw, np.array([0,0,0]), np.array([0,0,1]), 1, r, wavy_index = wavy_index ,num_circle = number_of_circles, rgba=(182, 183, 186, 255), width = 2)
+        file_name = save_dir + str(i) + '.png'
+        im.save(file_name)
+
+
 if __name__ == "__main__":
     # Create the directories to save image files
     save_dir_list = ['Images\\RotatingSphereDense\\im', 'Images\\RotatingSphereLight\\im', 'Images\\RotatingOscillatingSphere\\im']
@@ -71,4 +87,8 @@ if __name__ == "__main__":
     # Oscillating sphere: Draw the sphere that consist of n circles
     #draw_oscillating_sphere('Images\\RotatingOscillatingSphere\\im', 20)
 
-    draw_wavy_sphere_wrapper('Images\\RotatingOscillatingSphere\\im', 60)
+    # Without Acceleration
+    #draw_wavy_sphere_wrapper('Images\\RotatingOscillatingSphere\\im', 60)
+
+    # With Acceleration
+    draw_wavy_sphere_accleration_wrapper('Images\\RotatingOscillatingSphere\\im', 60)
