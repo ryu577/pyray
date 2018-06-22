81862ddc9fab7e17bdbd1c3caffd17d0354a1712
Rohit Pandey <rohitpandey576@gmail.com>
2018-06-22 01:11:39 -0700

    Converted indentation to spaces in test file

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from rotation.rotation import *
 
 
 class TestRotationMethods(unittest.TestCase):
-	def test_upper(self):
-		# Rotating by 260 degrees should preserve the vector.
+    def test_upper(self):
+        # Rotating by 260 degrees should preserve the vector.
         r = rotation(3, np.pi*2)
         vec = np.random.uniform(size=3)
         vec_rot = np.dot(r, vec)
