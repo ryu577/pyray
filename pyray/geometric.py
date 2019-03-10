1f1a7dcf4dc1474902af944919e316679bbef3e3
Lionel Brouyère <lionel.brouyere@gmail.com>
2019-03-10 21:30:53 +0100

    Refactoring jarvis_convex_hull
    I tryied to understand this really intresting algorithm .. but it was a be  confuse for me.
    I guess this version is more clear for fresh eyes and respect pep8
    Unfortunately, I don't have a good environment to test, but I should work cause I didn't touched the logic. I just changed the naming and the block organization.

--- pyray/geometric.py
+++ pyray/geometric.py
@@ -3,49 +3,68 @@ import numpy as np
 
 def jarvis_convex_hull(points):
     """
-    Returns points in the order required for a convex hull. A native alternative to scipy.spatial.ConvexHull
+    Returns points in the order required for a convex hull.
+    A native alternative to scipy.spatial.ConvexHull
     args:
-        points: An array of numpy arrays representing the points to be convex hulled in any order.
+        points: An array of numpy arrays representing
+        the points to be convex hulled in any order.
     """
-    start_indx = np.argmax(points[:,0]) #Point with the highest y-coordinate
-    res = []
-    res.append( (points[start_indx][0], points[start_indx][1]) )
-    added_points = set()
-    added_points.add(start_indx)
+    start_index = np.argmax(points[:,0])  # Point with the highest y-coordinate
+    start_point = points[start_index]
+    result = [start_index[:]]
+    added_points = {start_index}
     while True:
-        for i in range(len(points)):
-            exit = True
-            if i != start_indx and i not in added_points:
-                signs = 0
-                threshold = len(points) - 2
-                for j in range(len(points)):
-                    if j != i and j != start_indx:
-                        m = (points[i][1] - points[start_indx][1] * 1.00001)/(points[i][0] - points[start_indx][0] * 1.00001)
-                        check = points[j][1] - points[start_indx][1] - m * (points[j][0] - points[start_indx][0])
-                        if abs(check) < 1e-2:
-                            if dist(points[start_indx],points[j]) > dist(points[start_indx],points[i]):
-                                threshold = threshold + 1
-                            else:
-                                threshold = threshold - 1
-                        elif  check > 0:
-                            signs = signs + 1
-                        else:
-                            signs = signs - 1
-                if abs(signs) >= threshold:
-                    exit = False
-                    res.append( (points[i][0], points[i][1]))
-                    added_points.add(i)
-                    start_indx = i
-                    break
-        if exit:
-            return res
+        for ref_index, ref_point in enumerate(points):
+            exit_ = True
+            if i == start_index or i in added_points:
+                continue
+
+            signs = 0
+            threshold = len(points) - 2
+            for compare_index, compare_point in enumerate(points):
+                if compare_index == ref_index or compare_index == start_index:
+                    continue
+                check = compare(start_point, ref_point, compare_point)
+                if abs(check) < 1e-2:
+                    dist_start_ref = dist(start_point, ref_point)
+                    dist_start_compare = dist(start_point, compare_point)
+                    if dist_start_compare > dist_start_ref:
+                        threshold = threshold + 1
+                    else:
+                        threshold = threshold - 1
+                    continue
+                signs = signs + 1 if check > 0 else signs - 1
+
+            if abs(signs) < threshold:
+                continue
+
+            exit_ = False
+            result.append(ref_index[:])
+            added_points.add(ref_index)
+            start_index = ref_index
+            break
+
+        if exit_:
+            return result
 
 
-def dist(pt1,pt2):
+
+def distance(pt1, pt2):
     """
     Simple 2d euclidean distances between two points.
     args:
         pt1: The first point.
         pt2: The second point.
     """
-    return (pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2
+    return (pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2
+
+
+def compare(first_point, ref_point, compare_point):
+    """
+    I don't know exaclty what does this formula... anybody can explain ?
+    """
+    x2, y2 = first_point
+    x1, y1 = ref_point
+    x3, y3 = compare_point
+    m = (y2 - (y1 * 1.00001)) / (x2 - (x1 * 1.00001))
+    return y3 - y1 - m * (x3 - x1)
