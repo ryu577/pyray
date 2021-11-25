1e2e5a251716d99eb905355e6260de74ce11c814
Rohit Pandey <rohitpandey576@gmail.com>
2021-11-25 10:43:34 -0800

    Black hole image

--- gifs/black_hole.py
+++ gifs/black_hole.py
@@ -11,29 +11,98 @@ if os.name == 'posix':
     basedir = 'Images/RotatingCube/'
 
 
-## Make circular image of black hole.
-base_y = 400
-base_x = 100
-n = 5
-scale = 300
+def heat_map(j):
+    if j < 10:
+        return "yellow"
+    elif j < 20:
+        return "orange"
+    elif j < 30:
+        return "orange"
+    else:
+        return "red"
 
-im = Image.new("RGB", (512, 512), (0,0,0))
+
+## The singularity of a black hole.
+im = Image.new("RGB", (2048, 2048), "black")
 draw = ImageDraw.Draw(im, 'RGBA')
+rot = general_rotation(np.array([1,0,0]), np.pi/2.0-0.1)
+#rot = np.eye(3)
+phi = np.pi/10
+#phi = 0
+for j in range(50):
+    cmplx = np.exp(-1j*phi)*(1+j/400)
+    generalized_arc(draw, r=rot, vec=np.array([0,0,1]),
+                    point=np.array([cmplx.real,cmplx.imag,0]),
+                    prcnt=0.6
+                    , rgba=heat_map(j))
 
-r = 50
+rot = np.eye(3)
+for j in range(50):
+    cmplx = np.exp(1j*np.pi+1j*phi)*(1+j/400)
+    generalized_arc(draw, r=rot, vec=np.array([0,0,1]),
+                    point=np.array([cmplx.real,cmplx.imag,0]),
+                    prcnt=0.4
+                    , rgba=heat_map(j))
+im.show()
 
-for x in range(50):
-    y = np.sqrt(r**2-x**2)
-    pt1 = [x+10, y]
-    y1 = np.sqrt(r**2-(x+1)**2)
-    pt2 = [x+11, y1]
-    draw.line((pt1[0], pt1[1], pt2[0], pt2[1]), 
-            fill = (120,120,255,255), width = 3)
+###########################
+##
+
+im = Image.new("RGB", (2048, 2048), "black")
+draw = ImageDraw.Draw(im, 'RGBA')
+#rot = general_rotation(np.array([1,0,0]), np.pi/2.0-0.1)
+rot = np.eye(3)
+phi = np.pi/10
+generalized_arc(draw, r=rot, vec=np.array([0,0,1]),
+                    point=np.array([1,0,0]),
+                    prcnt=1.0
+                    , rgba="red")
+
+im.show()
+
+
+########
+im = Image.new("RGB", (2048, 2048), "black")
+draw = ImageDraw.Draw(im, 'RGBA')
+draw; r=rot; vec=np.array([0,0,1]); point=np.array([1,0,0]); prcnt=1
+rgba=(255, 0, 0, 100);
+scale=200;
+shift=np.array([1000, 1000, 0]);
+width=5
+
+pt1 = np.dot(r, point)
+vec = vec / sum(vec**2)**0.5
+theta = np.pi * 2.0 / 80.0 * prcnt
+
+for i in range(0, 80):
+    if i<40:
+        r = general_rotation(np.array([1,0,0]), np.pi/2.0-0.2)
+        r1 = general_rotation(np.dot(r, vec), theta)
+    else:
+        r = np.eye(3)
+        r1 = general_rotation(np.dot(r, vec), theta)
+    pt2 = np.dot(r1, pt1)
+    draw.line(
+        (pt1[0] *
+            scale +
+            shift[0],
+            pt1[1] *
+            scale +
+            shift[1],
+            pt2[0] *
+            scale +
+            shift[0],
+            pt2[1] *
+            scale +
+            shift[1]),
+        fill=rgba,
+        width=width)
+    pt1 = pt2
+
 
 
 ###############################
-
-## Grid for black hole.
+## Warped spacetime grid for black hole.
 im_ind=0; scale=200; shift=np.array([1000,1000,0])
 
 r1 = np.eye(4)
@@ -56,11 +125,12 @@ while abs(z1) < 1/t:
     z1 -= 0.0001*ix**1.1
     z = z1
     rad = np.sqrt(-t/z-t**2)
-    generalized_circle2(draw, r=r, center=np.array([0,0,z]), 
-                    vec=np.array([0,0,1]), 
-                    radius=rad,
-                    rgba=(255,20,147,50))
+    generalized_circle2(draw, r=r, center=np.array([0,0,z]),
+                        vec=np.array([0, 0, 1]),
+                        radius=rad,
+                        rgba=(255,20,147,50))
 
+## Draw the event horizon.
 z=-0.05
 rad = np.sqrt(-t/z-t**2)
 generalized_circle2(draw, r=r, center=np.array([0,0,z]), 
