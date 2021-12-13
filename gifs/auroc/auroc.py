359237513534f236626ed5129940b0d189ccd968
Rohit Pandey <rohitpandey@Rohits-MacBook-Air.local>
2021-12-12 17:21:14 -0800

    Added AUROC plots for various scenes in the blog.

--- gifs/auroc/auroc.py
+++ gifs/auroc/auroc.py
@@ -18,40 +18,6 @@ if os.name == 'posix':
     font_loc = 'Arial.ttf'
 
 
-
-#############################################################################
-## Scene 9 - rotate scene.
-for i in range(11):
-    (im, draw) = binary_classificn_pts(4, r=planar_rotation(-np.pi/4*(i)/10))
-    im.save(basedir + "/im" + str(int(i)) + ".png")
-
-#############################################################################
-## Scene 10 - let it rain over me.
-
-ind = 11
-for t in range(180):
-    if t%5==0:
-        (im, draw) = binary_classificn_pts(5, r=planar_rotation(-np.pi/4), t1=t)
-        im.save(basedir + "/im" + str(int(ind)) + ".png")
-        ind+=1
-
-base = ind
-im.close()
-ind=0
-for t in range(185):
-    if t%5==0:
-        (im, draw) = binary_classificn_pts(5, r=planar_rotation(-np.pi/4), t1=180, t2=t)
-        im.save(basedir + "/im" + str(int(base+ind)) + ".png")
-        ind+=1
-
-base+=ind
-for t in range(10):
-    (im, draw) = binary_classificn_pts(5, r=planar_rotation(-np.pi/4), t1=180, t2=185)
-    im.save(basedir + "/im" + str(int(base+t)) + ".png")
-
-############################################
-
-
 def draw_alpha_beta_curve(draw,alpha=0.15865525393145707,effect=50,std=30,draw_curve=True):
     font = ImageFont.truetype(font_loc, 12)
     # Draw the axes first.
