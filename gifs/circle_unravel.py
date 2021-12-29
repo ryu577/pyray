860b71463e2729a85b1319b5c3571c0b8f3ba50c
Rohit Pandey <rohitpandey576@gmail.com>
2021-12-28 17:03:51 -0800

    Fixed code for unraveling circle

--- gifs/circle_unravel.py
+++ gifs/circle_unravel.py
@@ -10,11 +10,12 @@ if os.name == 'posix':
     font_loc = 'Arial.ttf'
 
 
-def circle(draw, center = (256,256), r=50, frac=1.0):
-    for t in np.arange(np.pi/2, 5/2*np.pi*frac, 0.01):
+def circle(draw, center = (256,256), r=50, frac=1.0, start_angle=0):
+    for t in np.arange(start_angle, start_angle+2*np.pi*frac+0.01, 0.01):
         pt1 = np.exp(-1j*t)*r + (center[0]+1j)+center[1]*(1j)
         pt2 = np.exp(-1j*(t+0.01))*r+(center[0]+1j)+center[1]*(1j)
         draw.line((pt1.real, pt1.imag, pt2.real, pt2.imag), fill=(255,0,0))
+
 
 
 r=30
@@ -24,9 +25,17 @@ for ix in range(11):
     im = Image.new("RGB", (512,512), "black")
     draw = ImageDraw.Draw(im, 'RGBA')
     t = np.pi*2*ix/10
-    center = center_o+ np.array([-t*r,r])
-    circle(draw, center, r, 1-t/2/np.pi)
+    center = center_o + np.array([-t*r,r])
+    circle(draw, center, r, 1-t/2/np.pi, np.pi/2)
     pt2 = center-np.array([0,r])
     draw.line((base_pt[0],base_pt[1]-r, center[0], center[1]-r), fill=(255,0,0))
     im.save(basedir + 'im' + str(ix) + '.png')
+
+
+
+im = Image.new("RGB", (512,512), "black")
+draw = ImageDraw.Draw(im, 'RGBA')
+r=30
+center = (256,256)
+circle(draw, center, r, 0.7, np.pi/2)
 
